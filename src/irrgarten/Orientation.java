2bf459e53f3afe4a12717332ddd7b546d93485df
JavierVidGar <javiervidgar@correo.ugr.es>
2026-04-08 18:22:46 +0200

    Added Orientation Enum

---
+++ src/irrgarten/Orientation.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package irrgarten;
+
+/**
+ * <b>Orientation Enum</b>
+ *
+ * <p>
+ * This enum defines the orientation of a character.
+ * </p>
+ *
+ * @author javye
+ * @version 1.0
+ */
+public enum Orientation {
+    /**
+     * Vertical
+     */
+    VERTICAL,
+    /**
+     * Horizontal
+     */
+    HORIZONTAL;
+}
